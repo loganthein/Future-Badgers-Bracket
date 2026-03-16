499e1a03391b73e2ad164ad5e47c21bf21aa46a0
loganthein <loganthein@users.noreply.github.com>
2026-03-16 05:39:41 +0000

    deploy: c2959d3e416db6e3e0c83bcded32f30752b304e2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Future Badgers Bracket Challenge — Configuration
 const CONFIG = {
-  GIST_TOKEN:            'GIST_TOKEN_HERE',
+  GIST_TOKEN:            localStorage.getItem('gist_token') || null,
   BRACKET_DATA_GIST_ID:  '798906720fa8cb351ffb485d9631a07f',
   PICKS_GIST_ID:         'ce956d289985b0f4e5228f3fe4ade758',
   BRACKET_DATA_FILENAME: 'bracket-data.json',
